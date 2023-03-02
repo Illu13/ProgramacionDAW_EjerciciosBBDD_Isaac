2b5ce9275f7695d9d0a358c466c0e52836efa7c7
Illu13 <isaacdelgado442@gmail.com>
2023-03-02 11:16:23 +0100

    feat(utils): added last essential archives in order to start working

--- src/com/jds/centroEducativo/utils/JDBCPropiedades.java
+++ src/com/jds/centroEducativo/utils/JDBCPropiedades.java
@@ -23,7 +23,7 @@ public class JDBCPropiedades {
 			propiedades = new Properties();
 
 			try {
-				File file = new File("./src/capitulo07/jdbc2.properties");
+				File file = new File("./src/com.jds.centroEducativo/utils/jdbc2.properties");
 				propiedades.load(new FileReader(file));
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
